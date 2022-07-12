14bac8eb6582e52be3f3b78ffeac0380d79babce
matthias <meretzmatthias@yahoo.de>
2022-07-12 20:07:17 +0200

    Add component

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,13 @@
+import "../../Assets/Styles/Main.css"
+
+const Main = () => {
+
+
+  return (
+    <div className="Main">
+      Main
+    </div>
+  )
+}
+
+export default Main
---
+++ src/Components/Sidebar/Side.js
@@ -0,0 +1,13 @@
+import "../../Assets/Styles/Side.css"
+
+const Side = () => {
+
+
+  return (
+    <div className="Side">
+      Side
+    </div>
+  )
+}
+
+export default Side
