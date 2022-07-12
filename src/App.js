1cb6da0e6e04e2371c7af9321031c7862f9bfbaa
matthias <meretzmatthias@yahoo.de>
2022-07-12 20:04:33 +0200

    Add Side and Main components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Main from './Components/Main/Main';
+import Side from './Components/Sidebar/Side';
 
 function App() {
   return (
     <div className="App">
+      <Side/>
+      <Main/>
     </div>
   );
 }
